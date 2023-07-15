1d5354fa51f9827d33807157031ccf54eaaac6e9
Angela Niederberger <angelaniederberger@protonmail.ch>
2023-07-15 17:12:44 +0200

    minor changes

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -16,7 +16,7 @@ def get_args():
         description='Encrypt Phone Numbers',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('text',
                         metavar='str',
                         help='the text to be processed')
 
@@ -28,12 +28,16 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    text = args.positional
+    text = args.text
     encoding_dict = {"1": "9", "2": "8", "3": "7", "4": "6", "5": "0",
                      "6": "4", "7": "3", "8": "2", "9": "1", "0": "5"}
+
     new_chars = [encoding_dict.get(char, char) for char in text]
     print("".join(new_chars))
 
+    # Another way:
+    #[print(encoding_dict.get(char, char), end="") for char in text]
+    #print()
 
 # --------------------------------------------------
 if __name__ == '__main__':
